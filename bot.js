f69a0177be27bad0ab6f04d927491ea83da3e12d
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2023-03-15 03:09:37 -0400

    started the login process, added screenshot for testing purposes. Still need to add navigation to jobs page

---
+++ bot.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+
+(async () => {
+	// Launch a new browser instance
+	const browser = await puppeteer.launch();
+
+	// Open a new page and navigate to the LinkedIn login page
+	const page = await browser.newPage();
+	await page.goto("https://www.linkedin.com/login");
+
+	// Fill in the username and password fields using the environment variables
+	await page.type("#username", process.env.USERNAME);
+	await page.type("#password", process.env.PASSWORD);
+
+	// Submit the login form
+	await page.click('button[type="submit"]');
+
+	// Wait for the page to load after logging in
+	await page.waitForNavigation();
+	await page.waitForTimeout(5000);
+
+	// Take a screenshot of the logged-in homepage
+	await page.screenshot({ path: "linkedin_homepage.png" });
+
+	// Close the browser instance
+	await browser.close();
+})();
